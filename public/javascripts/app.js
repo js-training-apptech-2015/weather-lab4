821af6b5e9d030727f1f01aaa4578f89e20b4b26
Ivan Gromov <summer.is.gone@gmail.com>
2014-11-20 01:02:21 +0500

    Simple BB complete app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,51 @@
 (function ($, Backbone) {
     "use strict";
+    var IndexView = Backbone.View.extend({
+        template: "<h1>Searching {{search}}...</h1> <form><input name='search' /></form>",
+        render: function () {
+            var output = this._template.render(this.model.toJSON());
+            this.$el.html(output);
+            return this;
+        },
+        initialize: function () {
+            this._template = Hogan.compile(this.template);
+            this.render();
+            this.model.on('change', this.render, this);
+        },
+        events: {
+            "submit form": "onSearch"
+        },
+        onSearch: function (e) {
+            var query = this.$("input").val();
+            this.model.set({
+                search: query
+            });
+            return false;
+        }
+    });
+
+    var App = Backbone.Router.extend({
+        initialize: function (options) {
+            this.container = options.container;
+        },
+        routes: {
+            '': "index"
+        },
+        index: function () {
+            var search = new Backbone.Model();
+            var indexView = new IndexView({
+                el: this.container,
+                model: search
+            });
+        }
+    });
+
     $(function () {
-        var View = Backbone.View.extend({
-            render: function () {
-                this.$el.html('<h1>Hello world</h1>');
-            }
+        var app = new App({
+            container: $('#container')
         });
-        var view = new View({
-            el: $('#container')
+        Backbone.history.start({
+            pushState: false
         });
-        view.render();
     });
 })(jQuery, Backbone);
