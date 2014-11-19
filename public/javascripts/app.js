8647cd145d821cfca6b5449357780bd78b34d718
Ivan Gromov <summer.is.gone@gmail.com>
2014-11-20 01:01:26 +0500

    Simple BB View

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,14 @@
 (function ($, Backbone) {
     "use strict";
     $(function () {
-        console.log('Hello!');
+        var View = Backbone.View.extend({
+            render: function () {
+                this.$el.html('<h1>Hello world</h1>');
+            }
+        });
+        var view = new View({
+            el: $('#container')
+        });
+        view.render();
     });
 })(jQuery, Backbone);
